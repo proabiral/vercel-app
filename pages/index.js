0fa862b8d479be7913b8f72e8d33437f439b7e5a
Your Name <you@example.com>
2021-08-28 17:36:16 +0545

    1

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+   "8b8890fddfebb2dae2bbf1fc800ed727"
+  )
+}
