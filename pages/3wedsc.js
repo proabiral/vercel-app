67938c4decbee816d2fc56ecf34f55605c15f0c0
proabiral <22173232+proabiral@users.noreply.github.com>
2021-11-02 16:26:46 +0545

    Create 3wedsc.js

---
+++ pages/3wedsc.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+   "65f7c8c51c0104ec322b1dba9a06e609"
+  )
+}
